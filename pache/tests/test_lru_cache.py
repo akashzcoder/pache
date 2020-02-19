df1b6362f712665a70b2a2f9c28d8520190518ae
Akash Singh <akashsingh09.03.93@gmail.com>
2020-02-18 23:15:42 -0500

    add test case to check instance values

--- pache/tests/test_lru_cache.py
+++ pache/tests/test_lru_cache.py
@@ -7,3 +7,13 @@ def test_dq_node():
     expected_dq_node_str = f"{key} -> {value}"
     dq_node = lru_cache.DQNode(key=key, value=value)
     assert str(dq_node) == expected_dq_node_str
+
+
+def test_dq_node_instance():
+    key = "legend"
+    value = "Vegeta"
+    dq_node = lru_cache.DQNode(key=key, value=value)
+    assert dq_node.key == key
+    assert dq_node.value == value
+    assert dq_node.next is None
+    assert dq_node.prev is None
